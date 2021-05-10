f9f2fd7938e7af1ff6f79783bb22d6233ea19c38
4gnosed <gnosed@foxmail.com>
2021-05-10 18:34:36 +0800

    反射实时监控hashmap扩容参数

--- src/main/java/com/example/demo/DemoTest.java
+++ src/main/java/com/example/demo/DemoTest.java
@@ -7,6 +7,7 @@ import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -56,8 +57,31 @@ public class DemoTest extends RecursiveTask<Long> implements Comparator<Integer>
 
 
     public static void main(String[] args) throws Exception {
+        getCapacity();
+    }
 
+    static void getCapacity() throws Exception{
 
+        //指定初始容量15来创建一个HashMap
+        HashMap m = new HashMap(15);
+        //获取HashMap整个类
+        Class<?> mapType = m.getClass();
+        //获取指定属性，也可以调用getDeclaredFields()方法获取属性数组
+        Field threshold = mapType.getDeclaredField("threshold");
+        //将目标属性设置为可以访问
+        threshold.setAccessible(true);
+        //获取指定方法，因为HashMap没有容量这个属性，但是capacity方法会返回容量值
+        Method capacity = mapType.getDeclaredMethod("capacity");
+        //设置目标方法为可访问
+        capacity.setAccessible(true);
+        //打印刚初始化的HashMap的容量、阈值和元素数量
+        System.out.println("容量：" + capacity.invoke(m) + "    阈值：" + threshold.get(m) + "    元素数量：" + m.size());
+        for (int i = 0; i < 100; i++) {
+            m.put(i, i);
+            //动态监测HashMap的容量、阈值和元素数量
+            System.out.println("容量：" + capacity.invoke(m) + "    阈值：" + threshold.get(m) + "    元素数量：" + m.size());
+
+        }
     }
 
 
