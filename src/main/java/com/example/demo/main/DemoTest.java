a504289fa2b0519c5d1a3318302c2b2a1c29293e
gnosed <gnosed@foxmail.com>
2021-09-29 11:40:31 +0800

    mybatis generator 生成lombok注解的实体

--- src/main/java/com/example/demo/main/DemoTest.java
+++ src/main/java/com/example/demo/main/DemoTest.java
@@ -1,12 +1,12 @@
 package com.example.demo.main;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.cglib.beans.BeanCopier;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
@@ -46,6 +46,7 @@ class Banana extends Fruit {
 
 @Transactional
 @Slf4j
+@Component
 public class DemoTest extends RecursiveTask<Long> implements Comparator<Integer> {
 
     private static ForkJoinPool forkJoinPool = new ForkJoinPool();
@@ -67,8 +68,10 @@ public class DemoTest extends RecursiveTask<Long> implements Comparator<Integer>
     }
 
     public static void main(String[] args) throws Exception {
-              User user = (User) setterByReflect(User.class);
-        System.out.println(JSON.toJSONString(user));
+        String key ="rechargeNotice:RC892478522495475712:600";
+        int index = key.lastIndexOf(":");
+        String key_ = key.substring(0,index);
+        System.out.println(key_);
     }
 
     private static Object setterByReflect(Class<?> aClass){
