e9777d189a23184d7df28eabf8f59013bbb38be1
gnosed <gnosed@foxmail.com>
2021-10-09 18:23:00 +0800

    单元测试guava retry

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.demo;
 
+import com.github.rholder.retry.*;
+import com.google.common.base.Predicates;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @SpringBootTest
 class DemoApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    void test() {
+        guavaRetry();
+
+    }
+
+
+    private static void guavaRetry() {
+        Retryer<Integer> retryer = RetryerBuilder.<Integer>newBuilder()
+                .retryIfResult(Predicates.isNull())
+                .retryIfResult(Predicates.equalTo(2))
+                .retryIfExceptionOfType(IndexOutOfBoundsException.class)
+                .withStopStrategy(StopStrategies.stopAfterAttempt(3))
+                .withWaitStrategy(WaitStrategies.fixedWait(1,TimeUnit.SECONDS))
+                .withRetryListener(new RetryListener() {
+                    @Override
+                    public <V> void onRetry(Attempt<V> attempt) {
+                        log.info("listening...");
+                        if(attempt.hasException()){
+                            log.error("RetryListener exception:{}",attempt.getExceptionCause());
+                        }
+                    }
+                })
+                .build();
+
+        try {
+            retryer.call(()->{
+                return  2;
+            });
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (RetryException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
